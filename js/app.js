ca004b12b0d4e960bc3a6b2f681a2ccef663ab90
davidsah1991 <45639152+davidsah1991@users.noreply.github.com>
2019-09-20 10:35:45 -0500

    Tenant Name Updated

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ document.addEventListener('deviceready',onDeviceReady, false);
 
 var MyCampusApp = {
     config : {
-        tenant : "OSCC",
+        tenant : "BELMONT",
         serverUrl : "https://kryptos.kryptosmobile.com",
         tenantFolder : function(device, tenant) {
             if(device.platform == 'Android') {
